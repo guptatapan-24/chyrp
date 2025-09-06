9a1371de8624f5ca1ccf9406639ee9302727c50d
Tapan Gupta <tapangupta.2401@gmail.com>
2025-09-06 16:05:37 +0530

    Update API base URL to use environment variable

--- modern-chyrp/utils/api.js
+++ modern-chyrp/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
+
 });
 
 export default api;
